513dc7a520353b2e4116a26204f1857a578edb43
wher0001 <richardswheatley@hotmail.com>
2017-04-06 14:19:35 -0400

    Fixed to simply get and put data
    Using a global Class

--- Getting Started/src/serial_test.py
+++ Getting Started/src/serial_test.py
@@ -2,13 +2,21 @@ import serial
 
 class SerialWrapper:
     def __init__(self):
-        self.ser = serial.Serial('COM1', 115200, timeout=1)
+        self.ser = serial.Serial()
+        
+    def portOpen(self):
+        self.ser.open()
 
     def sendData(self, data):
         self.ser.write(data.encode())
-
-    def getData(self, size):
-        return self.ser.read(size)
+        
+    def getData(self, read_size):
+        return self.ser.read(read_size)
+        
+    def set_params(self, device, baud, time_out):
+        self.ser.baudrate = baud
+        self.ser.port = device
+        self.ser.timeout = time_out
 
 # Setup compass to output data with a checksum.        
 def setup_compass_output():
@@ -20,8 +28,10 @@ def get_serial_value(size):
 compass_serial = SerialWrapper()
 
 def main():
+    compass_serial.set_params('COM1', 115200, 1)
+    compass_serial.portOpen()
     setup_compass_output()
-    data = get_serial_value(100)
+    data = compass_serial.getData(100)
     print(data.decode());
 
 if __name__ == '__main__':
